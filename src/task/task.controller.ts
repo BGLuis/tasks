930d32b0f75fbbe072259047537f011f86baef18
BGLuis <luishenrique.gbarbosa21@gmail.com>
2024-12-18 23:21:00 -0300

    implement permissions

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -20,31 +20,37 @@ import { Permissions } from 'src/auth/decorator/permission.decorator';
 export class TaskController {
 	constructor(private readonly taskService: TaskService) {}
 
+	@Permissions('task-create')
 	@Post()
 	create(@Body() createTaskDto: CreateTaskDto) {
 		return this.taskService.create(createTaskDto);
 	}
-	@Permissions('admin')
+
+	@Permissions('task-read')
 	@Get()
 	findAll() {
 		return this.taskService.findAll();
 	}
 
+	@Permissions('task-read')
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return this.taskService.findOne(id);
 	}
 
+	@Permissions('task-update')
 	@Patch(':id/checkd')
 	check(@Param('id') id: string) {
 		return this.taskService.checkd(id);
 	}
 
+	@Permissions('task-update')
 	@Patch(':id')
 	update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
 		return this.taskService.update(id, updateTaskDto);
 	}
 
+	@Permissions('task-delete')
 	@Delete(':id')
 	remove(@Param('id') id: string) {
 		return this.taskService.remove(id);
